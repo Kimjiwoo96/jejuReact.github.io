c1f247cdb99d9ae4a38844d99f6b1af882e4bc7a
hyebine <berryme015@gmail.com>
2023-12-13 17:13:31 +0900

    작업 준비 중

--- src/App.js
+++ src/App.js
@@ -3,16 +3,30 @@ import Festival_P from "./pages/Festival_P";
 import {festivalObj , tourisObj} from "./js/commonData"
 
 
+//bine 작업
+import Hd from "./component/bine/Hd"
+
+
+
 function App() {
 
   const wrap ={border:"1px solid red", boxSizing:"border-box",width:"1280px",margin:"0 auto"}
 
   return (
+
+
+    <>
+    <Hd></Hd>
+   
+
+
     <div id="App" style={wrap}>
       <Festival_P
         commonData={festivalObj}
       ></Festival_P>
     </div>
+
+    </>
   );
 }
 
---
+++ src/component/bine/MainBanner.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Mainbanner() {
+  return (
+    <>
+    </>
+  )
+}
+
+export default Mainbanner
