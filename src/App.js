f47099c868d388bfa297a151bab4824bd544e198
Kimjiwoo96 <117326598+Kimjiwoo96@users.noreply.github.com>
2023-12-15 15:52:05 +0900

    비디오 영역

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@
 import {festivalObj , tourisObj ,eventObj} from "./js/commonData"
 import Festival_P from "./pages/Festival_P";
 import Event_P from "./pages/Event_P";
+import Video_P from "./pages/Video_p"
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <Festival_P
         commonData={festivalObj}
       ></Festival_P>
+      <Video_P></Video_P>
       <Event_P
         Eventdata={eventObj}
       ></Event_P>
