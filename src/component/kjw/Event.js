06030d59f17d968ebc36cea27b623bfd1d154fe1
Kimjiwoo96 <117326598+Kimjiwoo96@users.noreply.github.com>
2023-12-15 14:55:08 +0900

    이벤트 영역

--- src/component/kjw/Event.js
+++ src/component/kjw/Event.js
@@ -1,15 +1,48 @@
+// module 
+import { useState } from "react";
+// 파일 import
 import EventStyles from "../../scss/kjw/Event.module.scss";
 
 
 function Event(Eventdata){
+
+    const [isHovering, setIsHovering] = useState(0);
+    const [mythis, setmyThis] = useState("");
+
+    let colors =""
+    if(isHovering == 1){
+        colors = "3px solid #0675C1";
+    }
+    if(isHovering == 2){
+        colors = "3px solid #D48121";
+    }
+    if(isHovering == 3){
+        colors = "3px solid #EBEBEB";
+    }
+    if(isHovering == 4){
+        colors = "3px solid #059B84";
+    }
+
+    console.log("이거야>>>>>",isHovering)
+
+
+
     return(
         <>
 
         <div className={EventStyles.eventWrap}>
-        {
+             {
                 Eventdata.Eventdata.Eventdata.map((el,idx) => {
+
+
                     return(
-                    <div className={EventStyles.innerDiv} key={`event ${idx}`}>
+                    <div 
+                    style={{border:`${colors}`}}
+                    className={EventStyles.innerDiv}
+                    key={`event ${idx}`}
+                    onMouseOver={() => setIsHovering(idx+1)}
+                    onMouseOut={() => setIsHovering(0)}
+                    >
                         <p><span>item0{idx + 1}</span></p>
                         <h4>{el.subTitle}</h4>
                         <h2>{el.mainTitle}</h2>
@@ -25,3 +58,18 @@ function Event(Eventdata){
 }
 
 export default Event;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
