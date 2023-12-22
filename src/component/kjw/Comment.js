485659fca38a0b037383157e9fd44febb8b5853e
wisejiwoo96 <wisejiwoo96@naver.com>
2023-12-22 16:31:31 +0900

    댓글 UI

--- src/component/kjw/Comment.js
+++ src/component/kjw/Comment.js
@@ -1,7 +1,21 @@
+import CommitStyle from "../../scss/kjw/Comment.module.scss"
+
 function Comment(){
     return(
         <>
-
+            <div className={CommitStyle.Comment}>
+                <form action="">
+                    <div className={CommitStyle.CommentWrite1}>
+                        <label for="nickname">이름/닉네임</label>
+                        <input type="text" id="nickname" name="nickname"/>
+                    </div>
+                    <div className={CommitStyle.CommentWrite2}>
+                        <textarea name="" id="" cols="30" rows="10" placeholder="내용을 적어주세요"></textarea>
+                        <button>등록</button>
+                    </div>
+                </form>
+                
+            </div>
         </>
     );
 }
---
+++ src/pages/Comment_P.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Comment from '../component/kjw/Comment';
+
+function Comment_P() {
+
+    const marginWrap ={border:"1px solid red", boxSizing:"border-box",width:"1380px",margin:"0 auto",padding:"100px 0"}
+    const marginBottom = {marginBottom:"100px",textAlign:"center"}
+    return (
+        <div style={marginWrap}>
+            <h4 style={marginBottom}>제주도 최고의 여행지를 이웃과 함께 공유해보세요!</h4>
+            <Comment></Comment>
+        </div>
+    )
+}
+
+export default Comment_P;
