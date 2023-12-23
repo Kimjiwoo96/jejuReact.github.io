308b6ebe9988c27dcce64cc046a9db03511cc273
wisejiwoo96 <wisejiwoo96@naver.com>
2023-12-23 17:16:00 +0900

    댓글 value값 받기

--- src/component/kjw/Comment.js
+++ src/component/kjw/Comment.js
@@ -1,17 +1,55 @@
+import { useState } from "react"
 import CommitStyle from "../../scss/kjw/Comment.module.scss"
 
 function Comment(){
+
+    const [nicknameValue,setNicknameValue] = useState("")
+    const [commentConText,setcommentConText] = useState("")
+
+    function commentRegistration(){
+        console.log("이거를 DB로 넘겨주면 됨>>>>>>",nicknameValue)
+        console.log("DB로 넘겨주면 됨>>>>>>",commentConText)
+        alert("잘 나오나???")
+        
+    }
+
+    const nicknameChange = (event) => {
+        setNicknameValue(event.target.value);
+        setcommentConText(event.target.value);
+      }
+
+      const commentConTextChange = (event) => {
+        setcommentConText(event.target.value);
+      }
+
+
     return(
         <>
             <div className={CommitStyle.Comment}>
                 <form action="">
                     <div className={CommitStyle.CommentWrite1}>
                         <label for="nickname">이름/닉네임</label>
-                        <input type="text" id="nickname" name="nickname"/>
+                        <input
+                         type="text"
+                         id="nickname"
+                         name="nickname"
+                         value={nicknameValue}
+                         onChange={nicknameChange}
+                         />
                     </div>
                     <div className={CommitStyle.CommentWrite2}>
-                        <textarea name="" id="" cols="30" rows="10" placeholder="내용을 적어주세요"></textarea>
-                        <button>등록</button>
+                        <textarea
+                        name=""
+                        id=""
+                        cols="30" rows="10"
+                        placeholder="내용을 적어주세요"
+                        onChange={commentConTextChange}
+                        ></textarea>
+                        <button
+                            onClick={() => {
+                                commentRegistration();
+                            }}
+                        >등록</button>
                     </div>
                 </form>  
             </div>
--- src/component/kjw/CommentCon.js
+++ src/component/kjw/CommentCon.js
@@ -2,9 +2,13 @@ import React from 'react'
 import CommentConStyle from "../../scss/kjw/CommentCon.module.scss"
 
 function CommentCon() {
+
+    
+
+
+
     return (
         <div className={CommentConStyle.commentCon}>
-
             <div className={CommentConStyle.commentinfo}>
                 <section>
                     <p className={CommentConStyle.userId}>이건내아이디</p>
@@ -13,14 +17,9 @@ function CommentCon() {
 
                 <span className={CommentConStyle.delBtn}>삭제</span>
             </div>
-
-
             <div className={CommentConStyle.commentCons}>
                 <p class="content">왜 안되는거야 나를 화나게 하지마</p>
             </div>
-
-
-
         </div>    
     )
 }
